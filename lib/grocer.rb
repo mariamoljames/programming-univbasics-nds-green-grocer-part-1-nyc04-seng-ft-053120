c6a1708c87c5cdb2ee9f89423b7a9201750a8d46
Mariamol James <mariamol.james@gmail.com>
2020-06-20 18:37:35 +0000

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,17 +1,30 @@
-def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
 
+def find_item_by_name_in_collection(name, collection)
+  answer=nil
+  count=0
+  while count<collection.length do
+    if collection[count][:item].eql?(name)
+      answer=collection[count]
+    end
+    count+=1
+  end
+  answer
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
+  answer=[]
+  total_item=[]
+  count=0
+  while count<cart.length do
+    total_item<<cart[count][:item]
+    count+=1
+  end
+  uniq_item=total_item.uniq
+  count=0
+  while count<uniq_item.length do
+    answer<<find_item_by_name_in_collection(uniq_item[count],cart)
+    answer[count][:count]=total_item.count(uniq_item[count])
+    count+=1
+  end
+  answer
 end
-
-
-  
